f82ff7b9e78182daec59f4d62ccb81610841a096
Nath <nathrijacobs@gmail.com>
2020-04-01 15:31:59 +0200

    generated question component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-// import { ResultComponent } from './test-result/test.result.component';
+// import { questionTempComponent } from './app/questionTemp/questionTemp.component';
 import { FormsModule } from '@angular/forms';
+import { QuestionComponent } from './question/question.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    QuestionComponent,
+    // questionTempComponent
   ],
   imports: [
     BrowserModule,
